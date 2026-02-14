06d2413e407ab9cff4b3c7a7d0cc52fdf02f062a
Karan Roshan <karanroshan91@gmail.com>
2026-02-14 19:56:59 +0530

    update the code

--- js/script.js
+++ js/script.js
@@ -3,22 +3,28 @@ const resetBtn = document.querySelector(".reset-btn");
 const hintTag = document.querySelector(".hint span");
 const typingInput = document.querySelector(".typing-input");
 const wrongLetter = document.querySelector(".wrong-letter span");
+const guessLeft = document.querySelector(".guess-left span");
 
-let word, incorrects = [], corrects = [];
+let word, incorrects = [], corrects = [], maxGuesses;
 
 function randomWord() {
     // getting random object from wordList
     let ranObj = wordList[Math.floor(Math.random() * wordList.length)];
     word = ranObj.word;        // getting word of random object
-    console.log(word);
+    maxGuesses = 10;
+    incorrects = [];
+    corrects = [];
 
     hintTag.innerHTML = ranObj.hint;
+    guessLeft.innerHTML = maxGuesses;
+    wrongLetter.innerHTML = incorrects;
 
     let html = "";
     for (let i = 0; i < word.length; i++) {
         html += `<input type="text" disabled>`
     }
     inputs.innerHTML = html;
+    typingInput.focus();
 
 }
 
@@ -28,7 +34,6 @@ function initGame(e) {
     let key = e.target.value.toLowerCase();
     if (
         key.match(/^[A-Za-z]+$/) && !incorrects.includes(` ${key}`) && !corrects.includes(key)) {
-        console.log(key);
         if (word.includes(key)) {         // if user letter found in the word
             for (let i = 0; i < word.length; i++) {
                 // showing matched letter in the input value
@@ -39,11 +44,30 @@ function initGame(e) {
             }
         }
         else {
+            maxGuesses--;
             incorrects.push(` ${key}`)
         }
+        guessLeft.innerHTML = maxGuesses;
+        wrongLetter.innerHTML = incorrects;
     }
-    wrongLetter.innerHTML = incorrects;
     typingInput.value = "";
+
+    setTimeout(() => {
+        if (corrects.length === word.length) {   // if user found all letters
+            alert(`Congrats! You found the word ${word.toLowerCase()}`)
+            randomWord();   // calling randomWord function, sothe game reset
+        }
+        else if (maxGuesses < 1) {        // if user couldn't found all letters
+            alert("Game over! You don't have remaining guesses.");
+
+            for (let i = 0; i < word.length; i++) {
+                // show all letters in the input
+                inputs.querySelectorAll("input")[i].value = word[i];
+            }
+
+            randomWord();
+        }
+    });
 
 }
 
